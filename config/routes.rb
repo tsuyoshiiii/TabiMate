31476b13385c0faa53e01eba8c93c7461aa5e920
EC2 Default User <ec2-user@ip-172-31-46-164.ap-southeast-2.compute.internal>
2025-09-26 11:39:22 +0000

    [Add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'members/show'
-  get 'members/edit'
   devise_for :members
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: :about
+  resources :members
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
