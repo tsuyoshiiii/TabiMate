d5414de89a79a4eabe5d46de32213ae819b8655c
EC2 Default User <ec2-user@ip-172-31-46-164.ap-southeast-2.compute.internal>
2025-09-28 00:13:57 +0000

    [Add]member editページ

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -9,12 +9,39 @@ class MembersController < ApplicationController
     end
   end
 
+  def index
+    @members = Member.all
+    @member = current_member
+  end
+
   def show
     @member = Member.find(params[:id])
     @posts = @member.posts
-    @post = Post.new
   end
 
   def edit
+    @member = Member.find(params[:id])
+    unless @member.id == current_member.id
+      redirect_to member_path(current_member)
+    else
+      @member = Member.find(params[:id])
+    end
   end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      flash[:notice] = "You have updated member successfully."
+      redirect_to member_path(@member)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :introduction)
+  end
+
 end
